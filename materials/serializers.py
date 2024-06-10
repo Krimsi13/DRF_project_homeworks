09fb4cf13fc62307c2f9ee36ba7fd5a78ef84fb4
Krimsi13 <aleksejkrimsi@gmail.com>
2024-06-10 22:34:11 +0300

    hw_24_2_1

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_count_lessons(self, obj):
+        return obj.lessons.all().count()
+
+    # def get_count_lessons(self, instance):
+    #     return instance.lessons.all().count()
 
 
 class LessonSerializer(ModelSerializer):
