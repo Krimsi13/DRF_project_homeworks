ccd91091482652024f7b152ed88c69cefa0bf865
Krimsi13 <aleksejkrimsi@gmail.com>
2024-06-11 21:19:36 +0300

    hw_24_2_3

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,8 +4,16 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer('lessons', many=True)
+    # lesson = LessonSerializer(source='lessons', many=True)
 
     class Meta:
         model = Course
@@ -16,9 +24,3 @@ class CourseSerializer(ModelSerializer):
 
     # def get_count_lessons(self, instance):
     #     return instance.lessons.all().count()
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
