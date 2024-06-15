17a894819986cbe3cbddc05e3f5d9d1d5c1f4a0e
Krimsi13 <aleksejkrimsi@gmail.com>
2024-06-15 16:16:05 +0300

    hw_25_1_3

--- materials/models.py
+++ materials/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# from users.models import User
+from config import settings
+
 NULLABLE = {"null": True, "blank": True}
 
 
@@ -9,6 +12,8 @@ class Course(models.Model):
         upload_to="materials/preview/", **NULLABLE, verbose_name="Картинка"
     )
     description = models.TextField(verbose_name="Описание курса")
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Владелец", help_text="Укажите владельца курса")
 
     class Meta:
         verbose_name = "Курс"
@@ -26,6 +31,8 @@ class Lesson(models.Model):
     course = models.ForeignKey(
         Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс", **NULLABLE
     )
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Владелец", help_text="Укажите владельца урока")
 
     class Meta:
         verbose_name = "Урок"
--- materials/views.py
+++ materials/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
-from users.permissions import IsModer
+from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
@@ -14,17 +15,29 @@ class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
 
     def get_permissions(self):
-        if self.action in ['create', 'destroy']:
+        if self.action == 'create':
             self.permission_classes = (~IsModer,)
         elif self.action in ['update', 'retrieve']:
-            self.permission_classes = (IsModer,)
+            self.permission_classes = (IsModer | IsModer,)
+        elif self.action == 'destroy':
+            self.permission_classes = (~IsModer & IsOwner,)
         return super().get_permissions()
+
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
 
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = (~IsModer,)
+    permission_classes = (~IsModer, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
 
 
 class LessonListApiView(ListAPIView):
@@ -35,14 +48,16 @@ class LessonListApiView(ListAPIView):
 class LessonRetrieveApiView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonUpdateApiView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = (~IsModer,)
+    permission_classes = (IsAuthenticated, IsOwner)
